76716e03deaf882ceeca39d033f26d97343c2ede
madhu-mini <madhuminiyar@gmail.com>
2020-10-12 19:24:06 +0530

    "Adding setup file for the repository."

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='Tame_Of_Thrones',
+    version='',
+    packages=['tests', 'tame_of_thrones', 'tame_of_thrones.constants', 'tame_of_thrones.exceptions'],
+    url='https://github.com/madhu-mini/Tame_Of_Thrones',
+    license='',
+    author='Madhusudan Miniyar',
+    author_email='madhuminiyar@gmail.com',
+    description=''
+)
