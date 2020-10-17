51ba0a0334cc6f63e57f074f61ea7b5647131639
Rahul Bagad <rahulbagad@hotmail.com>
2020-10-17 08:21:39 +0530

    Update universeExceptions.py

--- tame_of_thrones/exceptions/universeExceptions.py
+++ tame_of_thrones/exceptions/universeExceptions.py
@@ -6,7 +6,7 @@ class InvalidUniverseError(Exception):
         return repr(self.msg)
 
 
-class InvalidKingdomError(Exception):
+class InvalidKingdomError(Exception):        #file name is univerExceptions. Either rename the file Name to something generic or create seperate classes. Also is the file name is according to conventions?
     def __init__(self, msg=''):
         self.msg = "Invalid Kingdom: " + msg
 
