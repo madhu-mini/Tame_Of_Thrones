c4e30ec5415a5c14b9a1c1645a2b4de1714598c9
madhu-mini <madhuminiyar@gmail.com>
2020-10-21 12:58:12 +0530

    Refactored naming of the test.

--- tests/test_war.py
+++ tests/test_war.py
@@ -7,14 +7,14 @@ from tame_of_thrones.war import War
 
 class TestWar(TestCase):
 
-    def test_get_mapping_lowercase_string(self):
-        self.assertDictEqual({'a': 1, 'c': 3, 'b': 2}, War.get_string_mapping("abccbc"))
+    def test_get_character_count_map_of_lowercase_string(self):
+        self.assertDictEqual({'a': 1, 'c': 3, 'b': 2}, War.get_character_count_map("abccbc"))
 
-    def test_get_mapping_uppercase_string(self):
-        self.assertDictEqual({'p': 1, 'q': 2, 'r': 3}, War.get_string_mapping("RRRQQP"))
+    def test_get_character_count_map_of_uppercase_string(self):
+        self.assertDictEqual({'p': 1, 'q': 2, 'r': 3}, War.get_character_count_map("RRRQQP"))
 
-    def test_get_mapping_mixed_case_string(self):
-        self.assertDictEqual({'p': 1, 'q': 2, 'r': 3}, War.get_string_mapping("RrrQqp"))
+    def test_get_character_count_map_of_mixed_case_string(self):
+        self.assertDictEqual({'p': 1, 'q': 2, 'r': 3}, War.get_character_count_map("RrrQqp"))
 
     def test_result_win(self):
         kingdom_1 = Kingdom('LAND', 'PANDA')
